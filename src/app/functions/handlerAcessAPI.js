e9aafc4dd05ac28f8de1cc8aa9fa5621fd2bdab1
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-10-17 17:36:51 -0400

    commit 1710

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,54 +1,21 @@
 'use server'
 
-const lista = [
-        {name : "vivi",
-        email : "vivi@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-       },
-       {name : "mendes",
-        email : "mendes@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-       },
-       {name : "isabel",
-        email : "isabel@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-       },
-       {name : "marcelino",
-        email : "marcelino@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-       },
-       {name : "gabriel",
-        email : "gabriel@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-       },
-       {name : "picolo",
-        email : "picolo@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-       }
-] 
+const url = "https://aula-17-10-one.vercel.app"
 
-
-
-const getUserAuthenticated = (userLogin) => {
-    let usermach = {};
-
-    lista.map((user) =>{
-        if(user.email === userLogin.email && user.password === userLogin.password){
-            usermach = user
-        }
-    })
-    return usermach
+const getUserAuthenticated = async (user) => {
+    const responseOfApi = await fetch(url + "/user/authenticated",
+    {
+        method:"POST",
+        headers:{ "Content-type":"Application/json"},
+        body: JSON.stringify(user)
+    }
+    );
+    const userAuth = await responseOfApi.json();
+    return userAuth;
 }
 
 const getUsers = () =>{
-        return lista
-        
+
     }
      
 export { getUsers, getUserAuthenticated };
